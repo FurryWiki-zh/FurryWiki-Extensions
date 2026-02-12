05d31ae1cade81784b9711f7473180c90322ac63
海屿有燕 <zorua@vip.qq.com>
2026-02-12 22:52:20 +0800

    添加 LoadExtensions.php 文件以加载多个 MediaWiki 扩展

---
+++ LoadExtensions.php
@@ -0,0 +1,74 @@
+<?php
+
+// 防止在非 MediaWiki 环境（如 IDE 语法检查）下因函数未定义而报错
+if (!function_exists('wfLoadExtension')) {
+    function wfLoadExtension($name, $path = null) {
+    }
+}
+
+$furwikiExtensionsPath = "/www/wwwroot/mw-utils/FurryWiki-Extensions";
+$furwikiSettingsPath = "/www/wwwroot/mw-utils/FurryWiki-Settings";
+
+// A
+wfLoadExtension( 'AbuseFilter', "$furwikiExtensionsPath/AbuseFilter/extension.json" );
+
+// C
+wfLoadExtension( 'CategoryTree', "$furwikiExtensionsPath/CategoryTree/extension.json" );
+wfLoadExtension( 'Cite', "$furwikiExtensionsPath/Cite/extension.json" );
+wfLoadExtension( 'CiteThisPage', "$furwikiExtensionsPath/CiteThisPage/extension.json" );
+wfLoadExtension( 'CodeEditor', "$furwikiExtensionsPath/CodeEditor/extension.json" );
+wfLoadExtension( 'ConfirmEdit', "$furwikiExtensionsPath/ConfirmEdit/extension.json" );
+
+// D
+wfLoadExtension( 'DiscussionTools', "$furwikiExtensionsPath/DiscussionTools/extension.json" );
+
+// E
+wfLoadExtension( 'Echo', "$furwikiExtensionsPath/Echo/extension.json" );
+
+// G
+wfLoadExtension( 'Gadgets', "$furwikiExtensionsPath/Gadgets/extension.json" );
+
+// I
+wfLoadExtension( 'ImageMap', "$furwikiExtensionsPath/ImageMap/extension.json" );
+wfLoadExtension( 'InputBox', "$furwikiExtensionsPath/InputBox/extension.json" );
+
+// L
+wfLoadExtension( 'Linter', "$furwikiExtensionsPath/Linter/extension.json" );
+wfLoadExtension( 'LoginNotify', "$furwikiExtensionsPath/LoginNotify/extension.json" );
+
+// M
+wfLoadExtension( 'Math', "$furwikiExtensionsPath/Math/extension.json" );
+wfLoadExtension( 'MultimediaViewer', "$furwikiExtensionsPath/MultimediaViewer/extension.json" );
+
+// N
+wfLoadExtension( 'Nuke', "$furwikiExtensionsPath/Nuke/extension.json" );
+
+// O
+wfLoadExtension( 'OATHAuth', "$furwikiExtensionsPath/OATHAuth/extension.json" );
+
+// P
+wfLoadExtension( 'PageImages', "$furwikiExtensionsPath/PageImages/extension.json" );
+wfLoadExtension( 'ParserFunctions', "$furwikiExtensionsPath/ParserFunctions/extension.json" );
+wfLoadExtension( 'PdfHandler', "$furwikiExtensionsPath/PdfHandler/extension.json" );
+wfLoadExtension( 'Poem', "$furwikiExtensionsPath/Poem/extension.json" );
+
+// R
+wfLoadExtension( 'ReplaceText', "$furwikiExtensionsPath/ReplaceText/extension.json" );
+
+// S
+wfLoadExtension( 'Scribunto', "$furwikiExtensionsPath/Scribunto/extension.json" );
+wfLoadExtension( 'SecureLinkFixer', "$furwikiExtensionsPath/SecureLinkFixer/extension.json" );
+wfLoadExtension( 'SpamBlacklist', "$furwikiExtensionsPath/SpamBlacklist/extension.json" );
+wfLoadExtension( 'SyntaxHighlight_GeSHi', "$furwikiExtensionsPath/SyntaxHighlight_GeSHi/extension.json" );
+
+// T
+wfLoadExtension( 'TemplateData', "$furwikiExtensionsPath/TemplateData/extension.json" );
+wfLoadExtension( 'TextExtracts', "$furwikiExtensionsPath/TextExtracts/extension.json" );
+wfLoadExtension( 'Thanks', "$furwikiExtensionsPath/Thanks/extension.json" );
+wfLoadExtension( 'TitleBlacklist', "$furwikiExtensionsPath/TitleBlacklist/extension.json" );
+
+// V
+wfLoadExtension( 'VisualEditor', "$furwikiExtensionsPath/VisualEditor/extension.json" );
+
+// W
+wfLoadExtension( 'WikiEditor', "$furwikiExtensionsPath/WikiEditor/extension.json" );
