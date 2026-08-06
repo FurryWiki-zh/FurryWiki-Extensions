abc03d3df1aaef74a013ff4ef7b33d0ad0acb79e
海屿有燕 <zorua@vip.qq.com>
2026-08-06 12:41:22 +0800

    chore: 添加 JsonConfig 模型和数据源注册以支持 Chart 扩展

--- LoadExtensions.php
+++ LoadExtensions.php
@@ -152,6 +152,27 @@ wfLoadExtension( 'InterwikiExtracts', "$furwikiExtensionsPath/InterwikiExtracts/
 
 // J
 wfLoadExtension( 'JsonConfig', "$furwikiExtensionsPath/JsonConfig/extension.json" );
+/// JsonConfig 模型和数据源注册（Chart 扩展依赖 .chart / .tab 页面）
+$wgJsonConfigModels = $wgJsonConfigModels ?? [];
+$wgJsonConfigs = $wgJsonConfigs ?? [];
+
+$wgJsonConfigModels['Tabular.JsonConfig'] = 'JsonConfig\JCTabularContent';
+$wgJsonConfigs['Tabular.JsonConfig'] = [
+	'namespace' => 486,
+	'nsName' => 'Data',
+	'pattern' => '/.\.tab$/',
+	'license' => 'CC0-1.0',
+	'isLocal' => true,
+];
+
+$wgJsonConfigModels['Chart.JsonConfig'] = 'MediaWiki\Extension\Chart\JCChartContent';
+$wgJsonConfigs['Chart.JsonConfig'] = [
+	'namespace' => 486,
+	'nsName' => 'Data',
+	'pattern' => '/.\.chart$/',
+	'license' => 'CC0-1.0',
+	'isLocal' => true,
+];
 
 // L
 wfLoadExtension( 'LabeledSectionTransclusion', "$furwikiExtensionsPath/LabeledSectionTransclusion/extension.json" );
